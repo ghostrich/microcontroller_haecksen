0df342915a206337870b55d6ca55b3024ac017ca
ghostrich <wiebke.hurrelmann@gmx.de>
2021-01-01 18:21:00 +0100

    Update demo_contest_start.ino

--- demo_contest_start.ino
+++ demo_contest_start.ino
@@ -1,76 +1,72 @@
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <ESP8266WiFi.h>
+#include <U8g2lib.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C display(U8G2_R0, /* reset=*/ 16, /* clock=*/ 5, /* data=*/ 4);
 
-const unsigned char earth [] PROGMEM = {
-// 'earth', 24x24px
-0x03, 0xff, 0x80, 0x0c, 0x00, 0x70, 0x0c, 0x00, 0x70, 0x14, 0x00, 0x48, 0x63, 0xc0, 0x44, 0x60, 
-0x3f, 0x84, 0x40, 0x3f, 0x8e, 0x80, 0x00, 0x0a, 0xe1, 0xfe, 0x0a, 0x92, 0x01, 0x06, 0x92, 0x00, 
-0x82, 0x92, 0x00, 0x82, 0x92, 0x00, 0x82, 0x91, 0xc0, 0x82, 0x90, 0x20, 0x82, 0xe0, 0x21, 0x02, 
-0x80, 0x21, 0x02, 0x80, 0x21, 0x02, 0x60, 0x22, 0x04, 0x60, 0x1c, 0x04, 0x10, 0x00, 0x08, 0x0c, 
-0x00, 0x70, 0x0c, 0x00, 0x70, 0x03, 0xff, 0x80
+const unsigned char bike [] PROGMEM = {
+  // 'bike', 13x13px
+  0x00, 0x00, 0x66, 0x00, 0x3c, 0x08, 0x10, 0x0f, 0x10, 0x02, 0xf8, 0x03,
+  0x0c, 0x06, 0x1e, 0x0f, 0xb3, 0x19, 0xa1, 0x10, 0xb3, 0x19, 0x1e, 0x0f,
+  0x0c, 0x06
+};
+
+
+const unsigned char planet [] PROGMEM = {
+// 'planet', 30x22px
+  0x00, 0xF0, 0x03, 0x0F, 0x00, 0xF0, 0x03, 0x0F, 0x00, 0x0F, 0xFC, 0x30, 
+  0x00, 0x0F, 0xFC, 0x30, 0xC0, 0x00, 0xC3, 0x30, 0xC0, 0x00, 0xC3, 0x30, 
+  0xC0, 0xF0, 0xC0, 0x0C, 0xC0, 0xF0, 0xC0, 0x0C, 0x30, 0x0F, 0x00, 0x03, 
+  0x30, 0x0F, 0x00, 0x03, 0xF0, 0x00, 0x00, 0x03, 0xF0, 0x00, 0x00, 0x03, 
+  0x30, 0x00, 0x00, 0x03, 0x30, 0x00, 0x00, 0x03, 0xCC, 0x00, 0xC0, 0x00, 
+  0xCC, 0x00, 0xC0, 0x00, 0xC3, 0x00, 0xC0, 0x00, 0xC3, 0x00, 0xC0, 0x00, 
+  0x03, 0x0F, 0x3C, 0x00, 0x03, 0x0F, 0x3C, 0x00, 0xFC, 0xF0, 0x03, 0x00, 
+  0xFC, 0xF0, 0x03, 0x00, 
 };
 
 void setup() {
   Serial.begin(115200);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); 
-  }
+  display.begin();
 }
 
 void loop() {
   Serial.println(F("pixels"));
   for (int numberofpixels = 0; numberofpixels < 25; numberofpixels++) {
-    display.clearDisplay();
-    display.display();
     display.clearDisplay(); 
-    display.drawPixel(numberofpixels, numberofpixels, SSD1306_WHITE);
-    display.display();
+    display.drawPixel(numberofpixels, numberofpixels);
+    display.sendBuffer();
     delay(10);
   }
   
   Serial.println(F("circle"));
   for (int radius = 0; radius < 25; radius++) {
     display.clearDisplay();
-    display.display();
-    display.clearDisplay();
-    display.drawCircle(10, 10, radius, SSD1306_WHITE); 
-    display.display();
+    display.drawCircle(10, 10, radius); 
+    display.sendBuffer();
     delay(10);
   }
   Serial.println(F("rect"));
   for (int wh = 0; wh < 50; wh++) {
     display.clearDisplay();
-    display.display();
-    display.clearDisplay();
-    display.drawRect(50,5,wh,wh,SSD1306_WHITE); 
-    display.display();
+    display.drawFrame(50,5,wh,wh); 
+    display.sendBuffer();
     delay(10);
   }
   Serial.println(F("round rect"));
-  for (int wh = 0; wh < 40; wh++) {
+  for (int wh = 8; wh < 40; wh++) {
     display.clearDisplay();
-    display.display();
-    display.clearDisplay();
-    display.drawRoundRect(100, 10, wh, wh, 4, SSD1306_WHITE);
-    display.display();
+    display.drawRFrame(100, 10, wh, wh, 4);
+    display.sendBuffer();
     delay(10);
   }
 
 
   display.clearDisplay();
-  display.drawBitmap(10, 2, earth, 24, 24, 1);
-  display.display();
+  display.drawXBMP(0, 0, 30, 22, planet);
+  display.sendBuffer();
   delay(3000);
     
 }
