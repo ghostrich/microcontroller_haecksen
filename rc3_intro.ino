61fa951bdbb7bd775b0e5681122aeb87ebeee731
ghostrich <wiebke.hurrelmann@gmx.de>
2021-01-02 12:36:23 +0100

    switched to U8g2

--- rc3_intro.ino
+++ rc3_intro.ino
@@ -1,15 +1,11 @@
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <ESP8266WiFi.h>
+#include <U8g2lib.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C display(U8G2_R0, /* reset=*/ 16, /* clock=*/ 5, /* data=*/ 4);
 
 int hCenter = 64;                      // horizontal center of animation
 int vCenter = 16;                      // vertical center of animation
@@ -22,104 +18,138 @@ int interval[75] = {};                 // array used to track speed of each star
 
 const unsigned char rc3logo [] PROGMEM = {
   // rc3logo, 32x32px
-  0xff, 0xff, 0xff, 0xff, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 
-  0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 
-  0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 
-  0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 
-  0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 
-  0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x80, 0x00, 0x00, 0x01, 0x81, 0xfd, 0xfd, 0xfd, 
-  0x81, 0x85, 0x81, 0x8d, 0x81, 0x85, 0x80, 0x0d, 0x81, 0xfd, 0x80, 0xfd, 0x81, 0xf9, 0x80, 0x05, 
-  0x81, 0x99, 0x81, 0x8d, 0x81, 0x8d, 0xfd, 0xfd, 0x80, 0x00, 0x00, 0x01, 0xff, 0xff, 0xff, 0xff
+  0xFF, 0xFF, 0xFF, 0xFF, 0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 
+  0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 
+  0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 
+  0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 
+  0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 
+  0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 
+  0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 
+  0x01, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00, 0x80, 0x81, 0xBF, 0xBF, 0xBF, 
+  0x81, 0xA1, 0x81, 0xB1, 0x81, 0xA1, 0x01, 0xB0, 0x81, 0xBF, 0x01, 0xBF, 
+  0x81, 0x9F, 0x01, 0xA0, 0x81, 0x99, 0x81, 0xB1, 0x81, 0xB1, 0xBF, 0xBF, 
+  0x01, 0x00, 0x00, 0x80, 0xFF, 0xFF, 0xFF, 0xFF
 };
 
 const unsigned char rocket [] PROGMEM = {
   // 'brandy_rockets_2, 14x32px
-  0x03, 0x00, 0x07, 0x80, 0x1f, 0xc0, 0x3f, 0xf0, 0x3f, 0xf0, 0x78, 0x78, 0x70, 0x38, 0xe0, 0x1c, 
-  0xe0, 0x1c, 0xf0, 0x3c, 0xf8, 0x7c, 0xfc, 0xfc, 0xff, 0xfc, 0xff, 0xfc, 0xff, 0xfc, 0xff, 0xfc, 
-  0xff, 0xfc, 0xff, 0xfc, 0x7f, 0xf8, 0x7f, 0xf0, 0x4f, 0xf0, 0x47, 0xd0, 0xc7, 0x98, 0x87, 0x88, 
-  0x87, 0x88, 0x83, 0x08, 0x83, 0x08, 0x83, 0x18, 0xc3, 0x18, 0x43, 0x10, 0x03, 0x00, 0x03, 0x00
+  0xC0, 0x00, 0xE0, 0x01, 0xF8, 0x03, 0xFC, 0x0F, 0xFC, 0x0F, 0x1E, 0x1E, 
+  0x0E, 0x1C, 0x07, 0x38, 0x07, 0x38, 0x0F, 0x3C, 0x1F, 0x3E, 0x3F, 0x3F, 
+  0xFF, 0x3F, 0xFF, 0x3F, 0xFF, 0x3F, 0xFF, 0x3F, 0xFF, 0x3F, 0xFF, 0x3F, 
+  0xFE, 0x1F, 0xFE, 0x0F, 0xF2, 0x0F, 0xE2, 0x0B, 0xE3, 0x19, 0xE1, 0x11, 
+  0xE1, 0x11, 0xC1, 0x10, 0xC1, 0x10, 0xC1, 0x18, 0xC3, 0x18, 0xC2, 0x08, 
+  0xC0, 0x00, 0xC0, 0x00
 };
 
 void setup() {
   Serial.begin(115200);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); 
-  }
+  display.begin();
+
 
   for (int i = 0; i < Quantity; i++) {          // loop to setup initial stars
     StarAngle[i] = random(360);                 // generates random angle for each star
     interval[i] = (random(4))+1;                // generates random speed for each star +1 keeps stars from being zero
   }  
 
-  
-  display.clearDisplay();
-  display.display();
-  display.clearDisplay();
-  display.drawBitmap(-24, 0, rc3logo, 32, 32, 1);
-  display.display();
+  display.setFont(u8g2_font_helvB12_tf);//u8g2_font_unifont_t_latin);
+
+}
+
+void loop() {
+  display.clearBuffer();
+  display.drawXBMP(-24, 0, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(100);
     
-  display.clearDisplay();
-  display.drawBitmap(-12, 0, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();
+  display.drawXBMP(-12, 0, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(100);
   
-  display.clearDisplay();  
-  display.drawBitmap(0, 0, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();  
+  display.drawXBMP(0, 0, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(100);
   
-  display.clearDisplay();
-  display.drawBitmap(12, 0, rc3logo, 32, 32, 1);
-  display.display();  
+  display.clearBuffer();
+  display.drawXBMP(12, 0, 32, 32, rc3logo);
+  display.sendBuffer();  
   delay(100);
   
-  display.clearDisplay();
-  display.drawBitmap(24, 0, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();
+  display.drawXBMP(24, 0, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(100);
   
-  display.clearDisplay();
-  display.drawBitmap(36, 0, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();
+  display.drawXBMP(36, 0, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(100);
   
-  display.clearDisplay();
-  display.drawBitmap(48, 0, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();
+  display.drawXBMP(48, 0, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(1000);
   
-  display.clearDisplay();
-  display.drawBitmap(48, -12, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();
+  display.drawXBMP(48, -12, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(100);
 
-  display.clearDisplay();
-  display.drawBitmap(48, -24, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();
+  display.drawXBMP(48, -24, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(100);
 
-  display.clearDisplay();
-  display.drawBitmap(48, -36, rc3logo, 32, 32, 1);
-  display.display();
+  display.clearBuffer();
+  display.drawXBMP(48, -36, 32, 32, rc3logo);
+  display.sendBuffer();
   delay(1000);
+
   
-  display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(20, 10);
-  display.println(F("haecksen"));
-  display.display();
+  
+  display.drawUTF8(15, 20, "haecksen.org");
+  display.sendBuffer();
   delay(3000);
 
-  display.clearDisplay();
-  display.drawBitmap(57, 0, rocket, 14, 32, 1);
-  display.display();
-  delay(3000);
+  display.clearBuffer();
+  display.drawXBMP(57, 0, 14, 32, rocket);
+  display.sendBuffer();
+  delay(100);
+
+  display.clearBuffer();
+  display.drawXBMP(57, -6, 14, 32, rocket);
+  display.sendBuffer();
+  delay(100);  
+
+  display.clearBuffer();
+  display.drawXBMP(57, -12, 14, 32, rocket);
+  display.sendBuffer();
+  delay(100);   
+
+  display.clearBuffer();
+  display.drawXBMP(57, -18, 14, 32, rocket);
+  display.sendBuffer();
+  delay(100);  
+
+  display.clearBuffer();
+  display.drawXBMP(57, -24, 14, 32, rocket);
+  display.sendBuffer();
+  delay(100); 
+
+  display.clearBuffer();
+  display.drawXBMP(57, -30, 14, 32, rocket);
+  display.sendBuffer();
+  delay(100);
+
+  display.clearBuffer();
+  display.drawXBMP(57, -36, 14, 32, rocket);
+  display.sendBuffer();
+  delay(100);
 
   for (int loops = 0; loops < 50; loops++) {
     unsigned long currentMillis = millis();
-    display.clearDisplay();                       // clear display
+    display.clearBuffer();                       // clear display
     for (int i = 0; i < Quantity; i++) {          // loop to draw each star
 
       if (StarProgress[i] > 70) {                // if statement to reset stars after going off screen
@@ -133,23 +163,16 @@ void setup() {
       if (currentMillis - StarProgress[i] >= interval[i] && StarProgress[i] > 0) {  // if statement to exclude some star paths
         Hstar = (hCenter + (sin(StarAngle[i] / 57.296) * StarProgress[i] ));        // calculates star horizontal coordinate
         Vstar = (vCenter + (cos(StarAngle[i] / 57.296) * StarProgress[i] ));        // calculates star horizontal coordinate
-        display.drawPixel(Hstar, Vstar, SSD1306_WHITE);
+        display.drawPixel(Hstar, Vstar);
       }
     }
-    display.display(); 
+    display.sendBuffer(); 
     delay(100);
     Serial.println(loops);
   }
-  display.clearDisplay();  
-  display.display();
+  display.clearBuffer();  
+  display.sendBuffer();
 
-  display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(20, 10);
-  display.println(F("have fun!"));
-  display.display();
-}
-
-void loop() {
-
+  display.drawUTF8(30, 20, "have fun!");
+  display.sendBuffer();
 }
